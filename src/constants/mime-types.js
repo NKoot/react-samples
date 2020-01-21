0bcdac7106587465641151e0ee97d6a110d103ff
Nata Koot <natakoot@gmail.com>
2020-01-21 10:24:18 +0400

    constants

---
+++ src/constants/mime-types.js
@@ -0,0 +1,8 @@
+export const pdf = 'application/pdf';
+export const doc = 'application/msword';
+export const docx = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document';
+export const xls = 'application/vnd.ms-excel';
+export const xlsx = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
+export const txt = 'text/plain';
+
+export const STATISTICS_UPLOAD_TYPES = [txt, doc, docx, xls, xlsx, pdf];
---
+++ src/constants/social.js
@@ -0,0 +1,15 @@
+const facebookUrl = process.env.FACEBOOK_URL;
+const telegramUrl = process.env.TELEGRAM_URL;
+const youtubeUrl = process.env.YOUTUBE_URL;
+const twitterUrl = process.env.TWITTER_URL;
+const linkedInUrl = process.env.LINKED_IN_URL;
+const instagramUrl = process.env.INSTAGRAM_URL;
+
+module.exports = {
+    facebookUrl,
+    telegramUrl,
+    youtubeUrl,
+    twitterUrl,
+    linkedInUrl,
+    instagramUrl
+};
