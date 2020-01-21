70c6f4da328b88eb479897ec9bb82cbf3c8b0b18
Nata Koot <natakoot@gmail.com>
2020-01-21 10:24:02 +0400

    Loader component

---
+++ src/components/Loader/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import styles from './loader.module.scss';
+
+const Loader = (props) => <div {...props} className={styles.loader}>Loading...</div>;
+export default Loader;
